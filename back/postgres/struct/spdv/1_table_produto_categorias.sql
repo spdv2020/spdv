833aacc1a0135b4a70daafd0a630cafb04a56522
spdv dev <spdv.uepg2020@gmail.com>
2021-04-05 16:39:33 -0300

    add fields

--- back/postgres/struct/spdv/1_table_produto_categorias.sql
+++ back/postgres/struct/spdv/1_table_produto_categorias.sql
@@ -2,10 +2,12 @@ CREATE TABLE spdv.produto_categorias
 (
   id bigserial NOT NULL,
   nome character varying(100) NOT NULL,
+  ativo boolean not null default true,
   data_criacao timestamp without time zone NOT NULL DEFAULT NOW(),
   data_atualizacao timestamp without time zone NOT NULL DEFAULT NOW(),
   PRIMARY KEY (id),
-  CONSTRAINT ukc_spd_pcat_nome UNIQUE (nome)
+  CONSTRAINT ukc_spd_pcat_nome EXCLUDE (nome WITH =) WHERE (ativo = true)
+  -- CONSTRAINT ukc_spd_pcat_nome UNIQUE (nome)
 );
 
 ALTER TABLE spdv.produto_categorias
